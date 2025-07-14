a109409dc70313672a17e4dc9055c23e66d8e52e
null <greatnull2@gmail.com>
2025-07-14 16:41:26 +0900

    baseUrl 수정

--- src/api/axiosInstance.tsx
+++ src/api/axiosInstance.tsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: '',
+  baseURL: 'https://bkmk-jl47.onrender.com/',
   headers: {
     'Content-Type': 'application/json',
   },
